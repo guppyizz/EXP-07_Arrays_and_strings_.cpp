f22a4225902454cb15daad166a259b903926a833
Updesh arora <updesh.arora4200@gmail.com>
2025-08-08 00:35:58 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,18 @@
+// UPDESH ARORA
+// PRN - 24070123124
+// ENTC B-3
+// EXPERIMENT 07
+
+#include<iostream>
+using namespace std;
+int main(){
+    int num[10];
+    cout <<"Enter any 5 integer values: ";
+    for(int i=0;i<5;i++){
+        cin>>num[i];
+    }
+    for(int j=0;j<5;j++)
+    {
+        cout<<num[j]<<" ";
+    }
+}
